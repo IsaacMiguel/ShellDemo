e33ffcbdabf41da48bcfd461e0650839e704212b
Isaac Miguel <isaacmiguel@gmail.com>
2016-05-14 05:21:18 -0300

    Update showme.php

--- application/controllers/showme.php
+++ application/controllers/showme.php
@@ -10,10 +10,6 @@ class Showme extends CI_Controller
 		$data['data'] = $this->show->get($suc);
 
 		print_r($data);
-
-		/*$conexion = mssql_connect('200.59.36.112', 'sa', 'pumitas', 'asuper') or die('Error al intentar conectarse a la base de datos.');
-		echo 'Conectado correctemente';
-		mssql_close($conexion);*/
 	}
 }
 ?>
